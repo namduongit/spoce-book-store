95147d6b40fb10a875e2e0bd6c56584c03825f6e
tien luan <tienluanchenh192@gmail.com>
2025-03-06 02:24:56 +0700

    Giao diện trang thanh toán (Chưa responsive)

--- public/index.php
+++ public/index.php
@@ -17,6 +17,7 @@
     <script src="js/detail_product.js" defer></script>
     <script src="js/login_register.js" defer></script>
     <script src="js/cart.js" defer></script>
+    <script src="js/payment.js" defer></script>
 </head>
 
 <body>
@@ -150,7 +151,7 @@
 
                             <tr class="topbar__cart-btn">
                                 <td class="topbar__cart"><button>Xem giỏ hàng</button></td>
-                                <td><button>Thanh toán</button></td>
+                                <td><button class="topbar__checkout-btn">Thanh toán</button></td>
                             </tr>
                         </table>
                     </div>
@@ -1029,7 +1030,7 @@
         </div>
     </main>
 
-    <div class="show-cart hide-item">
+    <div class="show-cart">
         <div class="show-cart__container">
             <div class="show-cart__title">
                 GIỎ HÀNG
@@ -1228,10 +1229,337 @@
                 </div>
                 <div class="show-cart__checkoutbox">
                     <button>Xóa tất cả</button>
-                    <button><i class="fa-regular fa-circle-check"></i> Thanh toán</button>
+                    <button class="show-cart__to-checkout-btn"><i class="fa-regular fa-circle-check"></i> Thanh toán</button>
                 </div>
             </div>
         </div>
+    </div>
+
+    <div class="checkout hide-item">
+        <div class="checkout__container">
+            <div class="checkout__customer-info-holder">
+                <!-- <div class="checkout__logo-holder">
+                    <a href="#"><img src="images/public_logo.png" alt="main-logo"></a>
+                </div> -->
+
+                <div class="checkout__customer-address-container">
+                    <p>Thông tin giao hàng</p>
+                    <div class="checkout__customer-address-field">
+                        <div class="checkout__address-field-one">
+                            <div class="checkout__input-field checkout__address-select">
+                                
+                                <select name="address-holder" id="address-holder">
+                                    <option value="default" selected>Địa chỉ đã lưu trữ</option>
+                                </select>
+                                <label for="address-holder">Thêm địa chỉ mới</label>
+                            </div>
+                            <div class="checkout__input-field">
+                                
+                                <input type="text" id="fullname" name="fullname" placeholder=" ">
+                                <label for="fullname">Họ và tên</label>
+                            </div>
+                            <div class="checkout__input-field">
+                                
+                                <input type="text" id="numberphone" name="numberphone" placeholder=" ">
+                                <label for="numberphone">Số điện thoại</label>
+                            </div>
+                            <div class="checkout__input-field">
+                                
+                                <input type="text" id="address" name="address" placeholder=" ">
+                                <label for="address">Địa chỉ</label>
+                            </div>
+                        </div>
+                        <div class="checkout__address-field-two">
+                            <div class="checkout__input-field checkout__address-select">
+                                <label>Tỉnh / thành</label>
+                                <select name="city" id="city">
+                                    <option value="default" selected>Chọn tỉnh / thành</option>
+                                </select>
+                            </div>
+                            <div class="checkout__input-field checkout__address-select">
+                                <label>Quận / huyện</label>
+                                <select name="district" id="district">
+                                    <option value="default" selected>Chọn quận / huyện</option>
+                                </select>
+                            </div>
+                            <div class="checkout__input-field checkout__address-select">
+                                <label>Phường / xã</label>
+                                <select name="ward" id="ward">
+                                    <option value="default" selected>Chọn phường / xã</option>
+                                </select>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="checkout__ship-container">
+                    <p>Phương thức vận chuyển</p>
+                    <div class="checkout__ship-method-holder">
+                        <div class="checkout__ship-method-unchoose" style="display: none;">
+                            <img src="images/download.svg" alt="package">
+                            <p>Vui lòng chọn tỉnh / thành để có danh sách phương thức vận chuyển.</p>
+                        </div>
+
+                        <div class="checkout__ship-method-choose">
+                            <label class="checkout__ship-method" for="tietkiem">
+                                <div class="checkout__ship-method-radiobtn-holder">
+                                    
+                                    <label class="checkout__ship-method-radiobtn">
+                                        <input type="radio" id="tietkiem" name="shipping-method" value="tietkiem">
+                                        <span></span>
+                                    </label>
+                                    <span>Giao hàng tiết kiệm</span>
+
+                                </div>
+
+                                
+                                <span>20.000đ</span>
+                            </label>
+
+                            <label class="checkout__ship-method" for="nhanh">
+                                <div class="checkout__ship-method-radiobtn-holder">
+                                    <label class="checkout__ship-method-radiobtn">
+                                        <input type="radio" id="nhanh" name="shipping-method" value="nhanh">
+                                        <span></span>
+                                    </label>
+                                    
+                                    <span>Giao hàng nhanh</span>
+                                </div>
+                                
+                                <span>30.000đ</span>
+                            </label>
+
+                            <label class="checkout__ship-method" for="hoatoc">
+                                <div class="checkout__ship-method-radiobtn-holder">
+                                    
+                                    <label class="checkout__ship-method-radiobtn">
+                                        <input type="radio" id="hoatoc" name="shipping-method" value="hoatoc">
+                                        <span></span>
+                                    </label>
+                                    <span>Giao hàng hỏa tốc</span>
+                                </div>
+                                
+                                <span>50.000đ</span>
+                            </label>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="checkout__payment-container">
+                    <p>Phương thức thanh toán</p>
+                    <div class="checkout__payment-method-holder">
+                        <label class="checkout__payment-method-option" for="cash">
+                            <div class="checkout__payment-method-radiobtn">
+                                <label class="checkout__payment-radiobtn-holder">
+                                    <input type="radio" id="cash" name="payment" value="cash">
+                                    <span></span>
+                                </label>
+                            </div>
+                            <div class="checkout__payment-method-content">
+                                <img src="images/cod.svg" alt="payment_image">
+                                <span>Thanh toán khi giao hàng (COD)</span>
+                            </div>
+                        </label>
+
+                        <label class="checkout__payment-method-option" for="qrcode">
+                            <div class="checkout__payment-method-radiobtn">
+                            
+                                <label class="checkout__payment-radiobtn-holder">
+                                    <input type="radio" id="qrcode" name="payment" value="qrcode">
+                                    <span></span>
+                                </label>
+                            </div>
+                            <div class="checkout__payment-method-content">
+                                <img src="images/type_atm.svg" alt="payment_image">
+                                <span>Chuyển khoản qua ngân hàng</span>
+                            </div>
+                        </label>
+
+                        <div class="checkout__qrcode-method-holder">  
+							<div class="checkout__qrcode-text">• NGÂN HÀNG THƯƠNG MẠI CỔ PHẦN NGOẠI THƯƠNG VIỆT NAM (VIETCOMBANK) - CHI NHÁNH SÀI GÒN
+                                • SỐ TÀI KHOẢN: 1013999802
+                                • TÊN TÀI KHOẢN: CTY CP TMDV SPOCE BOOK STORE	
+                            </div>											
+                        </div>
+
+                        <label class="checkout__payment-method-option" for="credit">
+                            <div class="checkout__payment-method-radiobtn">
+                                
+                                <label class="checkout__payment-radiobtn-holder">
+                                    <input type="radio" id="credit" name="payment" value="credit">
+                                    <span></span>
+                                </label>
+                            </div>
+                            <div class="checkout__payment-method-content">
+                                <img src="images/credit-card-removebg-preview.png" alt="payment_image">
+                                <div class="checkout__credit-content-holder">
+                                    <p>Thẻ Visa/Master/JCB/Amex/CUP</p>
+                                    <img src="images/visa_master_jcb_amex_cup.svg" alt="credit-cards">
+                                </div>
+                            </div>
+                        </label>
+
+                        <div class="checkout__credit-method-holder">
+                            <div class="checkout__input-field">
+                                <input type="tel" inputmode="numeric" pattern="[0-9\s]{13,19}" maxlength="19" id="card-number-field" name="card-number" placeholder="Số thẻ">
+                                <label>Số thẻ (16 số)</label>
+                            </div>
+
+                            <div class="checkout__input-field">
+                                <input type="tel" inputmode="numeric" maxlength="5" id="card-expiration-field" name="card-expiration" placeholder="Ngày hết hạn">
+                                <label>Ngày hết hạn (MM/YY)</label>
+                            </div>
+
+                            <div class="checkout__input-field">
+                                <input type="tel" inputmode="numeric" pattern="[0-9]{3}" maxlength="3" id="card-cvv-field" name="card-cvv" placeholder="Mã bảo mật">
+                                <label>Mã bảo mật (3 số)</label>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="checkout__cart-info-holder">
+                <div class="checkout__cart-products-holder">
+                    <table class="checkout__cart-table">
+                        <tbody>
+                            <tr>
+                                <td class="checkout__product-thumbnail">
+                                    <div class="checkout__product-image">
+                                        <div class="checkout__product-image-holder">
+                                            <img src="images/vo-van-kiet-nguoi-thap-lua-tb-2025.png" alt="product-image">
+                                        </div>
+                                        <span class="checkout__product-quantity">1</span>
+                                    </div>
+                                    
+                                </td>
+                                <td class="checkout__product-name">
+                                    <div class="checkout__product-name-holder">
+                                        <span>Người thắp lửa</span>
+                                        <span>893532502468 / Sách thiếu nhi / 220.000đ</span>
+                                    </div>
+                                </td>
+                                <td class="checkout__product-price">
+                                    <span>220.220đ</span>
+                                </td>
+                            </tr>
+
+                            <tr>
+                                <td class="checkout__product-thumbnail">
+                                    <div class="checkout__product-image">
+                                        <div class="checkout__product-image-holder">
+                                            <img src="images/vo-van-kiet-nguoi-thap-lua-tb-2025.png" alt="product-image">
+                                        </div>
+                                        <span class="checkout__product-quantity">1</span>
+                                    </div>
+                                    
+                                </td>
+                                <td class="checkout__product-name">
+                                    <div class="checkout__product-name-holder">
+                                        <span>Người thắp lửa</span>
+                                        <span>893532502468 / Sách thiếu nhi / 220.000đ</span>
+                                    </div>
+                                </td>
+                                <td class="checkout__product-price">
+                                    <span>220.220đ</span>
+                                </td>
+                            </tr>
+
+                            <tr>
+                                <td class="checkout__product-thumbnail">
+                                    <div class="checkout__product-image">
+                                        <div class="checkout__product-image-holder">
+                                            <img src="images/vo-van-kiet-nguoi-thap-lua-tb-2025.png" alt="product-image">
+                                        </div>
+                                        <span class="checkout__product-quantity">1</span>
+                                    </div>
+                                    
+                                </td>
+                                <td class="checkout__product-name">
+                                    <div class="checkout__product-name-holder">
+                                        <span>Người thắp lửa</span>
+                                        <span>893532502468 / Sách thiếu nhi / 220.000đ</span>
+                                    </div>
+                                </td>
+                                <td class="checkout__product-price">
+                                    <span>220.220đ</span>
+                                </td>
+                            </tr>
+
+                            <tr>
+                                <td class="checkout__product-thumbnail">
+                                    <div class="checkout__product-image">
+                                        <div class="checkout__product-image-holder">
+                                            <img src="images/vo-van-kiet-nguoi-thap-lua-tb-2025.png" alt="product-image">
+                                        </div>
+                                        <span class="checkout__product-quantity">1</span>
+                                    </div>
+                                    
+                                </td>
+                                <td class="checkout__product-name">
+                                    <div class="checkout__product-name-holder">
+                                        <span>Người thắp lửa</span>
+                                        <span>893532502468 / Sách thiếu nhi / 220.000đ</span>
+                                    </div>
+                                </td>
+                                <td class="checkout__product-price">
+                                    <span>220.220đ</span>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+
+                <div class="checkout__cart-promotion-holder">
+                    <form>
+                        <div class="checkout__promotion-input-holder">
+                            <div class="checkout__input-field">
+                                <input type="text" id="promotion-code" name="promotion-code" placeholder=" ">
+                                <label for="promotion-code">Mã giảm giá</label>
+                            </div>
+                        </div>
+                        <div class="checkout__promotion-btn-holder">
+                            <button class="checkout__promotion-btn">Sử dụng</button>
+                        </div>
+                    </form>
+                </div>
+
+                <div class="checkout__cart-submit-holder">
+                    <table>
+                        <tbody>
+                            <tr class="checkout__temp-total-holder">
+                                <td>Tạm tính</td>
+                                <td>
+                                    <span>220.000đ</span>
+                                </td>
+                            </tr>
+                            <tr class="checkout__shipping-fee-holder">
+                                <td>Phí vận chuyển</td>
+                                <td>
+                                    <span>—</span>
+                                </td>
+                            </tr>
+                        </tbody>
+                        <tfoot>
+                            <tr class="checkout__final-total-holder">
+                                <td>Tổng cộng</td>
+                                <td>
+                                    <span>VND</span>
+                                    <span>220.000đ</span>
+                                </td>
+                            </tr>
+                        </tfoot>
+                    </table>
+                    <div class="checkout__submit-btn-container">
+                        <span class="checkout__back-to-cart-btn">Giỏ hàng</span>
+                        <div class="checkout__submit-btn-holder">
+                            <button>Hoàn tất đơn hàng</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
     </div>
 
     <footer class="footer">
