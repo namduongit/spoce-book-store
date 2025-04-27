26e74ef94316c2ae1c21aa92762adda965ab6242
Nguyễn Nam Dương <96981636+namduongit@users.noreply.github.com>
2025-04-27 11:09:07 +0700

    test - Dương

---
+++ 404-Page/README.md
@@ -0,0 +1,63 @@
+<p align="center">
+  <img src="https://yt3.ggpht.com/Z5XPI05pZBU_eWSlGNe9OnoWvYnm5MLJWCrKn9xk77TrZz63m1DJqHyDsyWAlImwFi-0Xjl3IFQ=s176-c-k-c0x00ffffff-no-rj-mo" width="100px"/>
+  <h3 align="center" border-radius=".5rem" >Awesome 404 Page Template</h3>
+
+  <p align="center">
+    Watch Complete Tutorial on YT 
+    <br />
+    <br />
+    <a href="https://youtu.be/iXdj3Di_Lzo">
+      <img alt="youtube subscribers" width="150" height="40" title="Subscribe to my YouTube channel" src="https://img.shields.io/badge/Youtube-ea2845.svg?&style=for-the-badge&logo=Youtube&logoColor=white"/ target="blank"></a> 
+  
+  </p>
+</p>
+
+
+
+
+<div align="center">
+  <h3>
+    <a href="https://www.youtube.com/channel/UCK5YMqyy_fjAtwgu9hjxXJg">
+      Youtube
+    </a>
+    <span> | </span>
+    <a href="https://realisticcoder.github.io/404-Page/">
+        Live Preview
+    </a>
+  </h3>
+</div>
+
+<!-- TABLE OF CONTENTS -->
+
+## Table of Contents
+
+- [Overview](#overview)
+  - [Built With](#built-with)
+- [Contact](#contact)
+
+<!-- OVERVIEW -->
+
+## Overview
+
+![screenshot](https://raw.githubusercontent.com/RealisticCoder/404-Page/master/preview.png)
+
+Hi everyone, I have created this 404 template page. You can see the demo by  <a href="https://{your-url-to-the-solution}">
+        Clcking Here
+    </a> <br>
+You can use this template on your websites. 
+
+
+### Built With
+
+- [HTML](https://www.w3schools.com/html/)
+- [CSS](https://www.w3schools.com/css/)
+- [Javascript](https://www.w3schools.com/js/DEFAULT.asp)
+
+## Youtube
+Watch the Video Tutorial Here (With Voice Explanation) - [Realstic Coder](https://www.youtube.com/channel/UCK5YMqyy_fjAtwgu9hjxXJg)
+
+## Contact
+
+- Youtube - [Realstic Coder](https://www.youtube.com/channel/UCK5YMqyy_fjAtwgu9hjxXJg)
+- GitHub - [Realistic Coder](https://github.com/RealisticCoder/)
+
---
+++ 404-Page/index.php
@@ -0,0 +1,133 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Không tìm thấy trang bạn yêu cầu</title>
+  <link rel="stylesheet" href="404-Page/css/style.css">
+  <link rel="icon" type="image/png" href="../media/logo/human_book.png">
+
+</head>
+
+<body>
+
+  <div class="topbar">
+    <div class="topbar__container">
+      <div class="topbar__contact">
+
+        <div class="topbar__contact-item margin-right-medium">
+          <i class="topbar__contact-icon fa-solid fa-phone-volume"></i>
+          <span class="topbar__contact-text">0388.853.835</span>
+        </div>
+
+        <div class="topbar__contact-item margin-right-medium">
+          <i class="topbar__contact-icon fa-solid fa-envelope-open-text"></i>
+          <span class="topbar__contact-text">spoce_bookstore@gmail.com</span>
+        </div>
+
+        <div class="topbar__contact-item margin-right-medium">
+          <i class="topbar__contact-icon fa-solid fa-location-dot "></i>
+          <span class="topbar__contact-text">273 An Dương Vương, P2, Q5, TP.HCM</span>
+        </div>
+
+        <div class="topbar__contact-item margin-right-medium">
+          <i class="topbar__contact-icon fa-solid fa-envelope-open-text"></i>
+          <span class="topbar__contact-text">Địa chỉ: 273 An Dương Vương, Phường 2, Quận 5, Thành phố Hồ Chí Minh</span>
+        </div>
+
+      </div>
+    </div>
+
+  </div>
+
+  <div class="header">
+    <div class="header__container">
+      <a href="/" class="header__logo">
+        <img src="../media/logo/public_logo.png" alt="Logo Web">
+      </a>
+    </div>
+  </div>
+
+  <div class="page-not-found">
+    <header>
+      <div class="heading">
+        <h1>404 Not Found</h1>
+      </div>
+    </header>
+
+    <section class="home">
+      <div class="image">
+        <img src="404-Page/images/Scarecrow.png" alt="Scarecrow.png" width="100px">
+      </div>
+
+      <div class="content">
+        <h1>Có lỗi xảy ra trong trang hiện tại</h1>
+        <p>Hiện tại trang này chưa được chúng tôi bổ sung nên vẫn chưa hiển thị được, bạn vui lòng quay về trang chính của chúng tôi nhé</p>
+
+        <a href="/" class="btn">Quay về trang chính</a>
+      </div>
+
+    </section>
+
+  </div>
+
+
+  <footer class="footer">
+    <div class="footer__container container">
+      <div class="footer__top">
+        <div class="footer__top-container">
+          <div class="footer__top-item">
+            <h3>VỀ CÔNG TY</h3>
+            <ul>
+              <li class="footer__aboutus">Sản phẩm công nghệ của SpoceTech</li>
+              <li class="footer__contact">Số điện thoại: 0388853835</li>
+              <li class="footer__warranty">Email: nguyennamduong205@gmail.com</li>
+            </ul>
+          </div>
+          <div class="footer__top-item">
+
+          </div>
+          <div class="footer__top-item">
+            <h3>CHẤP NHẬN THANH TOÁN</h3>
+            <div class="footer__payment-image-container">
+              <img src="public/images/footer_logo_payment_1.png" alt="payment1">
+              <img src="public/images/footer_logo_payment_2.png" alt="payment2">
+              <img src="public/images/footer_logo_payment_3.png" alt="payment3">
+            </div>
+          </div>
+          <div class="footer__top-item">
+            <h3>ĐỐI TÁC VẬN CHUYỂN</h3>
+            <div class="footer__payment-image-container-two">
+              <img src="public/images/footer_logo_shipment_2.png" alt="shipment2">
+              <img src="public/images/footer_logo_shipment_3.png" alt="shipment3">
+            </div>
+          </div>
+          <div class="footer__top-item">
+            <h3>ĐỐI TÁC BÁN HÀNG</h3>
+            <div class="footer__payment-image-container-three">
+              <img src="public/images/footer_logo_seller_1.png" alt="seller1">
+              <img src="public/images/footer_logo_seller_2.png" alt="seller2">
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="footer__bottom">
+        <div class="footer__bottom-container">
+          <div>
+            <img src="public/images/footer_logobct.png" alt="logobct">
+          </div>
+          <div>
+            <h3>CÔNG TY CỔ PHẦN THƯƠNG MẠI DỊCH VỤ SPOCE GROUP</h3>
+            <p>Địa chỉ: 273 An Dương Vương, P2, Q5, TP.HCM MST:0303615027 do Sở Kế Hoạch Và Đầu Tư Tp.HCM cấp ngày 10/03/2011 Tel: 0388.853.835 Email: spoce_bookstore@gmail.com</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </footer>
+
+
+</body>
+
+</html>
