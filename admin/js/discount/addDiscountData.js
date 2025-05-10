097b5133c45c3318cba0e7431ccc99421c5c36c8
atuan41005 <tuan100205@gmail.com>
2025-05-10 09:51:07 +0700

    logic loại mã giảm giá

--- admin/js/discount/addDiscountData.js
+++ admin/js/discount/addDiscountData.js
@@ -166,8 +166,8 @@ export function addDiscountData() {
             (valueNum <= 0 || valueNum > 100)
           ) {
             errorMessage += "Giá trị phần trăm phải từ 0 đến 100!\n";
-          } else if (type === "Tiền" && valueNum <= 0) {
-            errorMessage += "Giá trị tiền phải lớn hơn 0!\n";
+          } else if (type === "Tiền" && valueNum <= 1000) {
+            errorMessage += "Giá trị tiền phải lớn hơn 1000!\n";
           }
         }
         if (!dateStart || !dateEnd) {
